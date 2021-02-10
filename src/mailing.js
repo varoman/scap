ae610ea77c73389fece4a3b6ba88e9ead112dc1d
varazdat manukyan <varazdat.manukyan@click2sure.co.za>
2021-02-10 12:50:02 +0400

    log

--- src/mailing.js
+++ src/mailing.js
@@ -23,6 +23,8 @@ module.exports.sendMail = async (content) => {
             cid: '63f28392-6aaf-11eb-9439-0242ac130002-futures-scrap-ho',
         }
     };
+    
+    console.log(mailOptions, 'mailOptions')
 
     await transporter.sendMail(mailOptions, function(error, info){
         if (error) {
