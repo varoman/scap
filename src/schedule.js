522b6e5c71cc0d1339d29315797d53d776bf6516
Varazdat Manukyan <varomanukyan@gmail.com>
2021-02-10 18:53:18 +0400

    Update schedule.js

--- src/schedule.js
+++ src/schedule.js
@@ -2,6 +2,6 @@ const schedule = require('node-schedule');
 const { scrap } = require('./scrap');
 
 
-module.exports.setScheduledJob = () => schedule.scheduleJob('0 10 9 * * *', () => {
+module.exports.setScheduledJob = () => schedule.scheduleJob('0 35 15 * * *', () => {
     scrap();
 });
